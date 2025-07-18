d220e1c8351cd8b9a4611c3771676ff5cef88079
shubham bhalala <140708994+shpatel123@users.noreply.github.com>
2025-07-18 18:43:06 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 mongoose
-  .connect("mongodb+srv://bhalalashubham1:<Sidog@29>@greencartcluster.7uv9qge.mongodb.net/")
+  .connect("mongodb+srv://bhalalashubham1:<Sidog@29>@greencartcluster.7uv9qge.mongodb.net/greencart")
   .then(() => {
     console.log("MongoDB connected successfully");
   })
